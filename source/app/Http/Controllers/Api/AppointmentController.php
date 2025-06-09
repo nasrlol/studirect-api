39f147924093a72e157ec0a997f3b37713fb976b
nasrlol <nsrddyn@gmail.com>
2025-06-09 22:32:17 +0200

    de CRUD operation uitgebouwd weer op basis van student en company

--- source/app/Http/Controllers/Api/AppointmentController.php
+++ source/app/Http/Controllers/Api/AppointmentController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use App\Models\Appointment;
+use Illuminate\Support\Facades\App;
 
 class AppointmentController extends Controller
 {
@@ -12,7 +15,8 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        //
+        $appointments = Appointment::all();
+        return response()->json(['data' => $appointments]);
     }
 
     /**
@@ -20,7 +24,19 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'student_id' => 'required|integer|exists:students,id',
+            'company_id' => 'required|integer|exists:companies,id',
+            // de exists:students,id kijkt na of dat de FK zich wel in de db bevindt
+            'time_slot' => 'required|string|max:255',
+        ]);
+
+        $appointment = Appointment::create($validate);
+
+        return response()->json([
+            'data'=> $appointment,
+            'message' => 'Appointment created successfully'
+        ], 201);
     }
 
     /**
@@ -28,7 +44,13 @@ class AppointmentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $appointment = Appointment::findOrFail($id);
+            return response()->json(['data'=>$appointment]);
+        } catch(ModelNotFoundException $e)
+        {
+            return response()->json(['message' => 'Appointment not found']);
+        }
     }
 
     /**
@@ -36,7 +58,17 @@ class AppointmentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $appointment = Appointment::findOrFail($id);
+        $validated = $request->validate([
+            'time_slot' => 'required|string|max:255',
+            // afspraak verzetten = enkel tijdstip verzetten
+        ]);
+
+        $appointment->update($validated);
+        return response()->json([
+            'data' =>  $appointment,
+            'message'=> 'Appointment updated successfully'
+        ], 204);
     }
 
     /**
@@ -44,6 +76,18 @@ class AppointmentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $appointment = Appointment::findOrFail($id);
+            $appointment->delete();
+
+            return response()->json([
+                'message' => 'Appointment deleted successfully'
+            ]);
+        }catch(ModelNotFoundException $e)
+        {
+            return response()->json([
+                'message' => 'Appointment not found']
+                , 404);
+        }
     }
 }
