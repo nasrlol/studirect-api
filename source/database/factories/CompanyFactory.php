dcf89e09ff9241c65acbb3c6f2b4777498df33f3
nasrlol <nsrddyn@gmail.com>
2025-06-09 17:18:20 +0200

    - onnodige code verwijdert - hoofdletters bij de fake data

--- source/database/factories/CompanyFactory.php
+++ source/database/factories/CompanyFactory.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CompanyFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     protected $model = Company::class;
 
     public function definition(): array
@@ -20,9 +15,9 @@ class CompanyFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->safeEmail(),
             'password' => 'password', // dit hoeft nog niet hier hashed te worden omdat ik de setAttribute functie ga maken die het wel gaat het hashen voor ons
-            'plan_type' => $this->faker->randomElement(['basic','standard', 'premium']),
+            'plan_type' => $this->faker->randomElement(['Basic','Standard', 'Premium']),
             'description' => $this->faker->paragraph(2),
-            'job_types' => $this->faker->randomElement(['stagair','fulltime','studentenjob']),
+            'job_types' => $this->faker->randomElement(['Stagair','Fulltime','Studentenjob']),
             'job_domain' => $this->faker->randomElement(['Artificial Intelligence', 'Software Development', 'Business Intelligence', 'Netwerken', 'Robotics']),
             'booth_location' => $this->faker->randomElement([
                 'A12', 'B5', 'C7', 'D3', 'E10']),
