3be5819ea8421bcdace1201a1f57d3335093a7ab
jarno-js <jarno.janssens@student.ehb.be>
2025-06-11 13:29:29 +0200

    Update Message.php

--- source/app/Models/Message.php
+++ source/app/Models/Message.php
@@ -3,27 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Message extends Model
 {
     // Toegestane velden bij mass-assignment
     protected $fillable = [
         'sender_id',
+        'sender_type',
         'receiver_id',
-        'content',
-        'timestamp',
+        'receiver_type',
+        'content'
     ];
 
     // Een bericht is verzonden door een gebruiker (student of bedrijf)
-    public function sender(): BelongsTo
+    public function sender(): MorphTo
     {
-        return $this->belongsTo(User::class, 'sender_id');
+        return $this->morphTo(__FUNCTION__, 'sender_type', 'sender_id');
     }
 
     // Een bericht is ontvangen door een gebruiker (student of bedrijf)
-    public function receiver(): BelongsTo
+    public function receiver(): MorphTo
     {
-        return $this->belongsTo(User::class, 'receiver_id');
+        return $this->morphTo(__FUNCTION__, 'receiver_type', 'receiver_id');
     }
 }
