4830d81d817a131d9e3a7afc2a1fface476a8631
nasrlol <nsrddyn@gmail.com>
2025-06-12 12:55:38 +0200

    changed the operation form get to a post

--- source/routes/api.php
+++ source/routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Api\MailController;
 // Mail routes
 // momenteel is dit nog maar een test mail om aan te tonen dat de server werkt
 // verdere implementatie moet nog gebeuren
-Route::get('/mail', [MailController::class, 'sendStudentVerification']);
+Route::post('/mail', [MailController::class, 'sendStudentVerification']);
 
 // Test route
 Route::get('/test', function() {
