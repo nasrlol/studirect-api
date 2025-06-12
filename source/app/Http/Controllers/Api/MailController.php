ec4092b5b136c738e4c6d3a06842f36c9ebc6c9b
nasrlol <nsrddyn@gmail.com>
2025-06-12 15:13:43 +0200

    appointment controller

--- source/app/Http/Controllers/Api/MailController.php
+++ source/app/Http/Controllers/Api/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Mail\StudentVerification;
+use App\Models\Appointment;
 use App\Models\Student;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -29,4 +30,18 @@ class MailController extends Controller
         }
 
     }
-}
+
+    public function AppointmentConfirmation(int $id): JsonResponse
+    {
+        $appointment = Appointment::findOrFail($id);
+
+        try {
+            Mail::to($appointment->email)->send(new StudentVerification($appointment));
+            return response()->json(['message' => 'Appointment confirmation mail sent']);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Failed to send confirmation email.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
