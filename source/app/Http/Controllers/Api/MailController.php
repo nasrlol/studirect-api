2ad6b1fdb48990428590679d0af1b859f03ef8c5
nasrlol <nsrddyn@gmail.com>
2025-06-12 14:46:36 +0200

    de controller voor company password mail ding op basis van student verification functie

--- source/app/Http/Controllers/Api/MailController.php
+++ source/app/Http/Controllers/Api/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Mail\StudentVerification;
+use App\Models\Company;
 use App\Models\Student;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -13,12 +14,30 @@ use Exception;
 
 class MailController extends Controller
 {
-    public function sendStudentVerification(int $id): JsonResponse
+    public function studentVerification(int $id): JsonResponse
     {
         $student = Student::findOrFail($id);
 
         try {
             Mail::to($student->email)->send(new StudentVerification($student));
+            return response()->json(['message' => 'Verification mail sent']);
+        } catch (Exception $e)
+        {
+            return response()->json([
+                'message' => 'Failed to send verification email.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+    }
+
+
+    public function companyPassword(int $id): JsonResponse
+    {
+        $company = Company::findOrFail($id);
+
+        try {
+            Mail::to($company->email)->send(new StudentVerification($company));
             return response()->json(['message' => 'Verification mail sent']);
         } catch (Exception $e)
         {
