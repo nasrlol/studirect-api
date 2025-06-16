0bf54723296c92bfdda1ffac4c2941438f2dcd4c
Dries-Michiels <driesmichiels73@gmail.com>
2025-06-16 15:53:03 +0200

    Admin HasApiTokens
    HasApiTokens toegevoegd aan Admin

--- source/app/Models/Admin.php
+++ source/app/Models/Admin.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = [
         'email',
         'password'
