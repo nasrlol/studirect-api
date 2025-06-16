7326c6651d5037b4108c0d91a6a830c7242b1dfb
Dries-Michiels <driesmichiels73@gmail.com>
2025-06-16 15:53:41 +0200

    HasApiTokens student
    HasApiTokens toegevoegd aan studenten

--- source/app/Models/Student.php
+++ source/app/Models/Student.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $fillable = [
         'first_name',
