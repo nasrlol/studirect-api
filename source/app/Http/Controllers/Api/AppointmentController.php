abb1d28f7c15de009383eb2cb6cf385bce5c29a1
nasrlol <nsrddyn@gmail.com>
2025-06-16 23:35:10 +0200

    Refactor relationship method names for consistency and clarity; update logging to include student ID in appointment creation

--- source/app/Http/Controllers/Api/AppointmentController.php
+++ source/app/Http/Controllers/Api/AppointmentController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
 use App\Services\LogService;
 use App\Services\MailService;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use App\Models\Appointment;
 
 class AppointmentController extends Controller
 {
@@ -37,7 +37,7 @@ class AppointmentController extends Controller
 
         // hier verzend ik de bevestigingsmail
         $mailService->sendAppointmentConfirmation($appointment);
-        $logger->setLog("student", "appointment creation", " appointment");
+        $logger->setLog("student", $appointment->student_id, "appointment creation", " appointment");
 
         return response()->json([
             'data' => $appointment,
