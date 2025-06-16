c53d61e73e2b68c023c57b90fbcc7a535986b235
Dries-Michiels <driesmichiels73@gmail.com>
2025-06-16 21:22:41 +0200

    Update company
    Fout met het dubbel hashen van een wachtwoord. Dit is nu opgelost door een check te doen

--- source/app/Models/Company.php
+++ source/app/Models/Company.php
@@ -36,13 +36,13 @@ class Company extends Model
     public function setPasswordAttribute($value)
     {
         // hashen enkel wanneer het nog niet gehashed is
-        /*if (!Hash::needsRehash($value)) {
+        if (Hash::needsRehash($value)) {
             $value = Hash::make($value);
         }
-        $this->attributes['password'] = $value;*/
+        $this->attributes['password'] = $value;
 
         // Altijd hashen om zeker te zijn
-        $this->attributes['password'] = Hash::make($value);
+        //$this->attributes['password'] = Hash::make($value);
     }
 
     // Relatie: een bedrijf kan meerdere afspraken hebben
