6ca1e2db3931f9a39834e3f4234ae83b0b07d755
Dries-Michiels <driesmichiels73@gmail.com>
2025-06-18 15:31:35 +0200

    Create Skill.php
    Nieuwe skill model aangemaakt

---
+++ source/app/Models/Skill.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Skill extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+    
+    /**
+     * The students that have this skill.
+     */
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class);
+    }
+    
+    /**
+     * The companies that require this skill.
+     */
+    public function companies(): BelongsToMany
+    {
+        return $this->belongsToMany(Company::class);
+    }
+}
