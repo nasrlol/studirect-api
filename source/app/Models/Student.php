6f12b7c9840519a808d0c7310103d6fe37b61595
Dries-Michiels <driesmichiels73@gmail.com>
2025-06-18 15:31:54 +0200

    Update Student.php
    Student model aangepast dat deze meerdere skills kan hebben

--- source/app/Models/Student.php
+++ source/app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
@@ -57,5 +58,10 @@ class Student extends Model
     public function diploma()
     {
         return $this->belongsTo(Diploma::class, 'graduation_track');
+    }
+
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class);
     }
 }
