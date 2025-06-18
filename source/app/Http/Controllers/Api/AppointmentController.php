eec189c25e048a7f411bae6c894f56505546c070
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-18 15:27:18 +0200

    functie om te checken of een nieuwe boeking binnen het start uur en eind uur van een andere booking valt

--- source/app/Http/Controllers/Api/AppointmentController.php
+++ source/app/Http/Controllers/Api/AppointmentController.php
@@ -24,6 +24,26 @@ class AppointmentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
+    private function appointmentTimeOverlap(array $data, $id = null): bool
+    {
+        $query = Appointment::where('company_id', $data['company_id'])
+            ->where(function ($query) use ($data) {
+                $query->whereBetween('time_start', [$data['time_start'], $data['time_end']])
+                    ->orWhereBetween('time_end', [$data['time_start'], $data['time_end']])
+                    ->orWhere(function ($query_) use ($data) {
+                        $query_->where('time_start', '<=', $data['time_start'])
+                            ->where('time_end', '>=', $data['time_end']);
+                    });
+            });
+
+        if ($id) {
+            $query->where('id', '!=', $id);
+        }
+
+        return $query->exists();
+    }
+
     public function store(Request $request, MailService $mailService, LogService $logService): JsonResponse
     {
         $validate = $request->validate([
