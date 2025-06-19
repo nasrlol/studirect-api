11c988ba7a990ee798f6144fead36c7349725b5b
nasrlol <nsrddyn@gmail.com>
2025-06-19 14:46:51 +0200

    Remove password hashing logic from Admin model

--- source/app/Models/Admin.php
+++ source/app/Models/Admin.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
@@ -17,14 +16,6 @@ class Admin extends Model
         'profile_photo'  // Add profile photo field
     ];
 
-    public function setPasswordAttribute($value)
-    {
-        // hashen enkel wanneer het nog niet gehashed is
-        if (Hash::needsRehash($value)) {
-            $value = Hash::make($value);
-        }
-        $this->attributes['password'] = $value;
-    }
     public function logging(): HasMany
     {
         return $this->HasMany(Log::class);
