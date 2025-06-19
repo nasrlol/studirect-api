f7bc77e54ab5333265d7682aa17c30c8def0316b
nasrlol <nsrddyn@gmail.com>
2025-06-19 19:19:38 +0200

    Refactor Student model to extend Authenticatable for enhanced authentication

--- source/app/Models/Student.php
+++ source/app/Models/Student.php
@@ -3,14 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-
-class Student extends Model
+class Student extends Authenticatable
 {
     use HasFactory, HasApiTokens;
 
