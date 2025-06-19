f4da5d561b4f48ea9a9ccdb7408ff19f62fe3f36
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-20 00:26:15 +0200

    Add AdminPolicy to define authorization rules for admin actions

---
+++ source/app/Policies/AdminPolicy.php
@@ -0,0 +1,41 @@
+namespace App\Policies;
+
+use App\Models\Admin;
+
+class AdminPolicy
+{
+public function viewAny(Admin $user): bool
+{
+return true;
+}
+
+public function view(Admin $user, Admin $admin): bool
+{
+return true;
+}
+
+public function create(Admin $user): bool
+{
+return true;
+}
+
+public function update(Admin $user, Admin $admin): bool
+{
+return true;
+}
+
+public function delete(Admin $user, Admin $admin): bool
+{
+return true;
+}
+
+public function restore(Admin $user, Admin $admin): bool
+{
+return false;
+}
+
+public function forceDelete(Admin $user, Admin $admin): bool
+{
+return false;
+}
+}
