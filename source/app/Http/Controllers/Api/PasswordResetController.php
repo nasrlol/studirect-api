359711572ecc45daf5b1ef118b546ef2c0048a64
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-22 23:09:53 +0200

    Refactor password reset logic to validate email input and improve student retrieval

--- source/app/Http/Controllers/Api/PasswordResetController.php
+++ source/app/Http/Controllers/Api/PasswordResetController.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Models\Student;
 use App\Services\MailService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\Mailer\Exception\TransportException;
 
 class PasswordResetController extends Controller
 {
 
-    public function sendResetStudentPassword(MailService $mailService, string $id): JsonResponse
+    public function sendResetStudentPassword(MailService $mailService, Request $request): JsonResponse
     {
-        $student = Student::findOrFail($id);
+        $validated = $request->validated([
+            'email' => 'required|email|exists:students,email',
+        ]);
 
+        $student = Student::where('email', $validated['email'])->firstOrFail();
         try {
             $mailService->sendStudentPasswordReset($student);
             return response()->json(['message' => 'Reset password mail send successfully']);
